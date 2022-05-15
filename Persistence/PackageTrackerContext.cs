1a566b2dc4c6209280af18f62f9f71e058b5bc90
Henrique Gomes de Andrade <hgomes.andrade@gmail.com>
2022-05-15 17:33:06 -0300

    Conectando com o SQL Server usando Migrations

--- Persistence/PackageTrackerContext.cs
+++ Persistence/PackageTrackerContext.cs
@@ -1,15 +1,36 @@
-﻿using PackageTrackerAPI.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using PackageTrackerAPI.Entities;
 
 namespace PackageTrackerAPI.Persistence
 {
-    public class PackageTrackerContext
+    public class PackageTrackerContext : DbContext
     {
-        public PackageTrackerContext()
-        {
-            Packages = new List<Package>();   
+        public PackageTrackerContext(DbContextOptions<PackageTrackerContext> options)
+            : base(options) 
+        { 
+
         }
 
-        public List<Package> Packages { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<PackageUpdate> PackageUpdates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Package>(e =>
+            {
+                e.HasKey(p => p.Id);
+
+                e.HasMany(p => p.Updates)
+                .WithOne()
+                .HasForeignKey(pu => pu.PackageId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            builder.Entity<PackageUpdate>(e =>
+            {
+                e.HasKey(pu => pu.Id);
+            });
+        }
     }
 }
     
--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using PackageTrackerAPI.Entities;
 using PackageTrackerAPI.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddSingleton<PackageTrackerContext>();
+
+var connectionString = builder.Configuration.GetConnectionString("PackageTrackerCS");
+builder.Services.AddDbContext<PackageTrackerContext>(c => c.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
